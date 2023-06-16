a9b29ca763a5ffc5176006583809f6985633f8a5
Anatolii <anatoliy.ocheretnyi@gmail.com>
2023-06-16 18:29:56 +0300

    fix: fixed review issue

--- src/pages/calculator/index.js
+++ src/pages/calculator/index.js
@@ -7,23 +7,29 @@ import { calculatorABI } from "../../utils/constants";
 import Bubbles from "../../components/bubbles";
 
 function App() {
-  const [operationMethod, setOperationMethod] = useState("");
+  const [operationMethod, setOperationMethod] = useState("add");
   const [isMetamaskConnected, setIsMetamaskConnected] = useState(false);
-  const [result, setResult] = useState("");
+  const [contractResult, setContractResult] = useState("");
   const [usageCount, setUsageCount] = useState("");
   const [digitA, setDigitA] = useState("");
   const [digitB, setDigitB] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const showResult = isLoading
+    ? "calculating in progress..."
+    : `Result: ${contractResult}`;
 
   const connectMetamask = async () => {
     if (typeof window.ethereum !== "undefined") {
       try {
         await window.ethereum.enable();
         setIsMetamaskConnected(true);
+        getUsageCount();
       } catch (error) {
-        console.error("Error connecting Metamask:", error);
+        console.error("Помилка при підключенні Metamask:", error);
       }
     } else {
-      console.error("Ethereum provider not found.");
+      console.error("Провайдер Ethereum не знайдено.");
     }
   };
 
@@ -34,7 +40,28 @@ function App() {
         const accounts = await web3.eth.requestAccounts();
         return accounts[0];
       } catch (error) {
-        console.error("Error retrieving sender address:", error);
+        console.error("Помилка при отриманні адреси відправника:", error);
+      }
+    } else {
+      console.error("Провайдер Ethereum не знайдено.");
+    }
+  };
+
+  const getUsageCount = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const web3 = new Web3(window.ethereum);
+      const contractAddress = "0x1851ffBce02A134eFd9ddBC91920b0c6DCEfB6f5";
+      const contract = new web3.eth.Contract(calculatorABI, contractAddress);
+
+      const senderAddress = await getSenderAddress();
+
+      try {
+        const count = await contract.methods
+          .usageCount()
+          .call({ from: senderAddress });
+        setUsageCount(count?.toString());
+      } catch (error) {
+        console.error("Error:", error);
       }
     } else {
       console.error("Ethereum provider not found.");
@@ -50,7 +77,6 @@ function App() {
       const a = parseInt(digitA);
       const b = parseInt(digitB);
 
-      // Додаткова перевірка на валідність числових значень
       if (isNaN(a) || isNaN(b)) {
         console.error("Invalid input. Please enter valid numbers.");
         return;
@@ -61,33 +87,56 @@ function App() {
       const senderAddress = await getSenderAddress();
 
       try {
+        setIsLoading(true);
         let result;
         if (methodName === "add") {
-          result = await contract.methods
-            .add(a, b)
-            .send({ from: senderAddress });
+          try {
+            await contract.methods.add(a, b).send({ from: senderAddress });
+            result = await contract.methods
+              .add(a, b)
+              .call({ from: senderAddress });
+          } catch (error) {
+            console.log("Transaction failed:", error);
+          }
         } else if (methodName === "subtract") {
-          result = await contract.methods
-            .subtract(a, b)
-            .send({ from: senderAddress });
+          try {
+            await contract.methods.subtract(a, b).send({ from: senderAddress });
+            result = await contract.methods
+              .subtract(a, b)
+              .call({ from: senderAddress });
+          } catch (error) {
+            console.log("Transaction failed:", error);
+          }
         } else if (methodName === "multiply") {
-          result = await contract.methods
-            .multiply(a, b)
-            .send({ from: senderAddress });
+          try {
+            await contract.methods.multiply(a, b).send({ from: senderAddress });
+            result = await contract.methods
+              .multiply(a, b)
+              .call({ from: senderAddress });
+          } catch (error) {
+            console.log("Transaction failed:", error);
+          }
         } else if (methodName === "divide") {
-          result = await contract.methods
-            .divide(a, b)
-            .send({ from: senderAddress });
+          try {
+            await contract.methods.divide(a, b).send({ from: senderAddress });
+            result = await contract.methods
+              .divide(a, b)
+              .call({ from: senderAddress });
+          } catch (error) {
+            console.log("Transaction failed:", error);
+          }
         }
 
         const count = await contract.methods
           .usageCount()
           .call({ from: senderAddress });
-        setResult(Number(result));
-
+        console.log(result);
+        setContractResult(Number(result));
         setUsageCount(Number(count));
       } catch (error) {
         console.error("Error:", error);
+      } finally {
+        setIsLoading(false);
       }
     } else {
       console.error("Ethereum provider not found.");
@@ -95,15 +144,22 @@ function App() {
   };
 
   useEffect(() => {
-    const checkMetamaskConnection = () => {
-      if (
-        typeof window.ethereum !== "undefined" &&
-        window.ethereum.isMetaMask &&
-        window.ethereum.isConnected()
-      ) {
-        setIsMetamaskConnected(true);
+    const checkMetamaskConnection = async () => {
+      if (typeof window.ethereum !== "undefined") {
+        const web3 = new Web3(window.ethereum);
+        try {
+          const accounts = await web3.eth.getAccounts();
+          if (accounts.length > 0) {
+            setIsMetamaskConnected(true);
+            getUsageCount();
+          } else {
+            setIsMetamaskConnected(false);
+          }
+        } catch (error) {
+          console.error("Помилка перевірки підключення Metamask:", error);
+        }
       } else {
-        setIsMetamaskConnected(false);
+        console.error("Провайдер Ethereum не знайдено.");
       }
     };
 
@@ -117,13 +173,13 @@ function App() {
   return (
     <div className="container container--bubbles">
       <div className="container button--conectMetamask">
-        {!isMetamaskConnected ? (
-          <button className="button--Metamask" onClick={connectMetamask}>
-            Connect Metamask
-          </button>
-        ) : (
+        {isMetamaskConnected ? (
           <button className="button--Metamask-connected">
             Metamask connected
+          </button>
+        ) : (
+          <button className="button--Metamask" onClick={connectMetamask}>
+            Connect Metamask
           </button>
         )}
       </div>
@@ -166,10 +222,10 @@ function App() {
 
         <div>
           {isMetamaskConnected && (
-            <span className="calculator__result">Result: {result}</span>
+            <span className="calculator__result">{showResult}</span>
           )}
         </div>
-
+        <div className="loader"></div>
         <div className="calculator__button">
           <button
             onClick={handleCalculate}
@@ -178,6 +234,7 @@ function App() {
           >
             Calculate
           </button>
+
           {isMetamaskConnected && (
             <label className="calculator__used-label" id="calculatorUsedLabel">
               Calculator used: {usageCount}
